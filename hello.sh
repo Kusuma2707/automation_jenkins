b1ba6543307caacc621d2583897edde1b614bc8c
Kusuma2707 <kusumakommana2707@gmail.com>
2025-07-23 15:41:56 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10, temp;
+
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    // Swapping
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
 echo "Hello   from GitHub to Jenkins!"
 
